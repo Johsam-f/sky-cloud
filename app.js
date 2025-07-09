bc9b4871d430f7e4ee37d0a2c5ccae23fd56739c
Johsam <johsamfcress@gmail.com>
2025-07-09 19:08:53 +0200

    installed connect-flash + session code orgased

--- app.js
+++ app.js
@@ -1,38 +1,60 @@
 require('dotenv').config();
 const express = require('express');
-const session = require('express-session');
 const path = require('path');
 const flash = require("connect-flash");
 const helmet = require('helmet');
 const morgan = require('morgan');
+const expressSession = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const { passport, attachUserToLocals } = require('./middlewares/auth');
 const Router = require('./routes/Router');
 
 const app = express();
+//ejs + static files
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Security & Logs
+app.use(helmet());
 //It applies a sensible default set of protections
 // helmet helps secure your Express app by setting various HTTP headers that protect against well-known web vulnerabilities.
-app.use(helmet());
 
+app.use(morgan('dev')); // 'dev' is a built-in log format
 // (Morgan) Great for debugging requests, routes, or spotting weird traffic patterns.
 //In production, it can help you analyze performance and request logs (especially if piped to a log file).
-app.use(morgan('dev')); // 'dev' is a built-in log format
 
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-}));
+//SESSION STORE USING PRISMA
+app.use(
+    expressSession({
+      cookie: {
+       maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+      },
+      secret: 'a santa at nasa',
+      resave: true,
+      saveUninitialized: true,
+      store: new PrismaSessionStore(
+        new PrismaClient(),
+        {
+          checkPeriod: 2 * 60 * 1000,  //ms = 2 minutes
+          dbRecordIdIsSessionId: true,
+          dbRecordIdFunction: undefined,
+        }
+      )
+    })
+);
+
+//PASSPORT AUTH
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(attachUserToLocals);
 
+//flash
 app.use(flash());
 app.use((req, res, next) => {
   res.locals.errorMessages = req.flash("error");
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,85 +2,62 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
 const { PrismaClient } = require("@prisma/client");
-const expressSession = require('express-session');
-const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
-
 const prisma = new PrismaClient();
 
 passport.use(
   new LocalStrategy(
     { usernameField: "email" },
     async (email, password, done) => {
-      try {
-        const user = await prisma.user.findUnique({
-          where: { email },
-        });
+        try {
+            const user = await prisma.user.findUnique({
+                where: { email },
+            });
 
-        if (!user) {
-          return done(null, false, { message: "Incorrect email" });
+            if (!user) {
+                return done(null, false, { message: "Incorrect email" });
+            }
+
+            const match = await bcrypt.compare(password, user.password);
+            if (!match) {
+                return done(null, false, { message: "Incorrect password" });
+            }
+            return done(null, user);
+        } catch (err) {
+            return done(err);
         }
-
-        const match = await bcrypt.compare(password, user.password);
-        if (!match) {
-          return done(null, false, { message: "Incorrect password" });
-        }
-
-        return done(null, user);
-      } catch (err) {
-        return done(err);
-      }
     }
   )
 );
 
-app.use(
-    expressSession({
-      cookie: {
-       maxAge: 7 * 24 * 60 * 60 * 1000 // ms
-      },
-      secret: 'a santa at nasa',
-      resave: true,
-      saveUninitialized: true,
-      store: new PrismaSessionStore(
-        new PrismaClient(),
-        {
-          checkPeriod: 2 * 60 * 1000,  //ms
-          dbRecordIdIsSessionId: true,
-          dbRecordIdFunction: undefined,
-        }
-      )
-    })
-  );
-
 // Serialize and deserialize
 passport.serializeUser((user, done) => {
-  done(null, user.id); 
+    done(null, user.id); 
 });
 
 passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await prisma.user.findUnique({
-      where: { id },
-    });
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id },
+        });
 
-    done(null, user);
-  } catch (err) {
-    done(err);
+        done(null, user);
+    } catch (err) {
+        done(err);
   }
 });
 
 const attachUserToLocals = (req, res, next) => {
-  res.locals.currentUser = req.user;
-  next();
+    res.locals.currentUser = req.user;
+    next();
 };
 
 const ensureAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) return next();
-  res.redirect("/login");
+    if (req.isAuthenticated()) return next();
+    res.redirect("/login");
 };
 
 module.exports = {
-  passport,
-  attachUserToLocals,
-  ensureAuthenticated,
+    passport,
+    attachUserToLocals,
+    ensureAuthenticated,
 };
