1c4458ee852ff1ec90d86ff6dd83c43c591bdb1a
Johsam <johsamfcress@gmail.com>
2025-07-09 18:23:33 +0200

    added Prisma session store library

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,6 +2,8 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
 const { PrismaClient } = require("@prisma/client");
+const expressSession = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
 
 const prisma = new PrismaClient();
 
@@ -30,6 +32,25 @@ passport.use(
     }
   )
 );
+
+app.use(
+    expressSession({
+      cookie: {
+       maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+      },
+      secret: 'a santa at nasa',
+      resave: true,
+      saveUninitialized: true,
+      store: new PrismaSessionStore(
+        new PrismaClient(),
+        {
+          checkPeriod: 2 * 60 * 1000,  //ms
+          dbRecordIdIsSessionId: true,
+          dbRecordIdFunction: undefined,
+        }
+      )
+    })
+  );
 
 // Serialize and deserialize
 passport.serializeUser((user, done) => {
